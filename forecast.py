cfde96ab442faf598da661bcb564d807c6a77af4
Jonathan Wheeler <atheistpiece@gmail.com>
2016-06-01 08:06:15 -0700

    Updated with more stuff

--- forecast.py
+++ forecast.py
@@ -1,19 +1,26 @@
-import json
 import requests
 from weather import Weather
 
 passwords = {}
-filename = "keys.txt"
+filename = "keys.txt" #we'll store our api keys here
+
+# create a dict with our api keys
 with open(filename, 'r') as f:
 	for line in f:
 		(key,val) = line.split('=')
 		passwords[key] = val.rstrip()
 
-apikey = str(passwords['wunderground'])
+apikey = passwords['wunderground']
+
+#get current conditions and 3 day forecast from wunderground
 conditions = requests.get('http://api.wunderground.com/api/' + apikey + '/conditions/q/CA/Santa_Ana.json')
 forecast = requests.get('http://api.wunderground.com/api/' + apikey + '/forecast/q/CA/Santa_Ana.json')
 
+#create a weather object with our two forecasts
 r = Weather(conditions.json(), forecast.json())
 
-new = r.get_forecast()
+#get the forecast
+todayWeather = r.get_forecast() #contains location, temp, weather, rain as keys
 
+for key in todayWeather:
+	print(todayWeather[key])
--- weather.py
+++ weather.py
@@ -1,10 +1,10 @@
 class Weather:
 	#setup the object, get two lists from two api calls to wunderground
+	#forecast1 = current conditions, forecast2 = 3 day forecast
+	#we can do more with the info later, for now, just the basics
 	def __init__(self, forecast1, forecast2):
 		self.forecast1 = forecast1['current_observation']
-		self.forecast2 = forecast2['forecast']
-
-#		print(self.forecast2, self.forecast1)
+		self.forecast2 = forecast2['forecast']	# this is needed for chance of rain, which isnt in current conditions
 
 	def get_temp(self):
 		return self.forecast1['temperature_string']
@@ -12,19 +12,22 @@ class Weather:
 	def get_weather(self):
 		return self.forecast1['weather']
 
-	def get_location(self):
-		pass
+	def get_location(self): #get city name
+		return self.forecast1['display_location']['full'] #nested dictionaries
 
 	def get_wind(self):
 		pass
 
 	def get_rain(self):
-		return self.forecast2['txt_forecast']['forecastday'][6]['pop']
+		return self.forecast2['txt_forecast']['forecastday'][6]['pop'] #we have a nested dictionarys, forecastday
+																	   #contains nested lists of dictionaries, confused?
 
-
+	#gather all the info we need to return the day's basic forecast
 	def get_forecast(self):
 		self.info = {}
 		self.info['temp'] = self.get_temp()
 		self.info['weather'] = self.get_weather()
 		self.info['rain'] = self.get_rain()
+		self.info['location'] = self.get_location()
+
 		return self.info
